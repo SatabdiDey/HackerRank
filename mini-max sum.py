d0526931104793b1b54330b0799bb08a02fe399e
satabdi <deysatabdi70@gmail.com>
2022-10-10 19:51:28 +0530

    hackerrank challenge 3

---
+++ mini-max sum.py
@@ -0,0 +1,24 @@
+'''import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#'''
+
+def miniMaxSum(arr):
+    x=sorted(arr)
+    print(sum(x[:-1]), sum(x[1:]))
+    
+    
+  
+'''
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)'''
